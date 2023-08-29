8a10937816ea2f89b45a1ecfd8b5ebaef45aaf79
Eidi <eidiyuki21@gmail.com>
2023-08-28 23:52:42 -0300

    Melhoras na Interface. Algoritmo de validação de senha adicionado. Todas as validações foram modificadas para funcionarem via JavaScript.

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validateInputs();
+
+  if (validateInputs() == true) {
+    //If the form is valid, then submit it.
+    form.submit();
+  }
 });
 
 function setError(element, message) {
@@ -42,60 +46,68 @@ const validateInputs = () => {
 
   if (firstNameValue === "") {
     setError(firstName, "Digite o nome");
+    return false;
   } else if (firstNameValue.length < 4 || firstNameValue.length > 15) {
     setError(firstName, "Nome deve conter de 4 a 15 letras");
+    return false;
   } else {
     setValid(firstName);
   }
 
   if (lastNameValue === "") {
     setError(lastName, "Digite o Sobrenome");
+    return false;
   } else if (lastNameValue.length < 4 || lastNameValue.length > 15) {
     setError(lastName, "Sobrenome deve conter de 4 a 15 letras");
+    return false;
   } else {
     setValid(lastName);
   }
 
   if (emailValue === "") {
     setError(email, "Digite o e-mail");
+    return false;
   } else if (!emailValue.match(emailRegex)) {
     setError(email, "E-mail inválido");
+    return false;
   } else {
     setValid(email);
   }
 
   if (phoneNumberValue === "") {
     setError(phoneNumber, "Digite o número do telefone");
+    return false;
   } else if (!phoneNumberValue.match(phoneRegex)) {
     setError(phoneNumber, "Número do telefone inválido");
+    return false;
   } else {
     setValid(phoneNumber);
   }
 
   if (passwordValue === "") {
     setError(password, "Digite a senha");
+    return false;
   } else if (!passwordValue.match(passwordRegex)) {
     setError(
       password,
       "A senha deve conter pelo menos 1 número, 1 letra maiúscula e minúscula, e pelo menos 8 ou mais caracteres. "
     );
+    return false;
   } else {
     setValid(password);
   }
 
   if (confirmPasswordValue === "") {
     setError(confirmPassword, "Digite a senha novamente");
+    return false;
   } else if (confirmPasswordValue === passwordValue) {
-    console.log(confirmPasswordValue);
-    console.log(passwordValue);
-
+    setValid(confirmPassword);
+  } else {
     setError(
       confirmPassword,
       "As duas senhas não se coincidem. Tente novamente"
     );
-  } else {
-    setValid(confirmPassword);
-    console.log(confirmPasswordValue);
-    console.log(passwordValue);
+    return false;
   }
+  return true;
 };
